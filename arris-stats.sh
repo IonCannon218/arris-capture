a215acd8283a3ed0ed0fd3070b494027d424e7cd
IonCannon218 <IonCannon218@users.noreply.github.com>
2016-11-06 16:40:38 -0500

    SB6141 and InfluxDB.

--- arris-stats.sh
+++ arris-stats.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
-# Script by graysky
-# https://github.com/graysky2/bin/blob/master/arris_signals
+#!/bin/bash
+# Original script by graysky https://github.com/graysky2/bin/blob/master/arris_signals
+# Modified for Arris SB6141 in 2016 by IonCannon218 https://github.com/IonCannon218/arris-capture
 #
 
 # PREFACE
@@ -37,7 +37,7 @@
 TEMP=/tmp/snapshot.html
 
 # LOGPATH is the full path to the log file you will keep.
-LOGPATH=/tmp/mnt/router/optware/share/www/arris
+LOGPATH=/tmp/arris-stats/
 
 # If you want to log all html snapshots set KEEPTHEM to any value
 # Be sure that you have sufficient storage for this as files are around 5 Kb
@@ -59,7 +59,7 @@ RUNDATE=$(date "+%F %T")
 [[ -f "$TEMP" ]] && rm -f "$TEMP"
 
 # try to get stats 6 times waiting 10 sec per time
-wget -q -T 10 -t 6 http://192.168.100.1/cgi-bin/status_cgi -O $TEMP || fail
+wget -q -T 10 -t 6 http://192.168.100.1/cmSignalData.htm -O $TEMP || fail
 
 if [[ -n "$KEEPTHEM" ]]; then
 	SAVE="$LOGPATH/snapshots"
@@ -69,112 +69,222 @@ if [[ -n "$KEEPTHEM" ]]; then
 fi
 
 # find downstream frequencies
-DF1=$(grep 'Downstream 1' $TEMP | awk -F'<td>' '{ print $4 }' | sed 's/<.*//')
-DF2=$(grep 'Downstream 2' $TEMP | awk -F'<td>' '{ print $13 }' | sed 's/<.*//')
-DF3=$(grep 'Downstream 3' $TEMP | awk -F'<td>' '{ print $22 }' | sed 's/<.*//')
-DF4=$(grep 'Downstream 4' $TEMP | awk -F'<td>' '{ print $31 }' | sed 's/<.*//')
-DF5=$(grep 'Downstream 5' $TEMP | awk -F'<td>' '{ print $40 }' | sed 's/<.*//')
-DF6=$(grep 'Downstream 6' $TEMP | awk -F'<td>' '{ print $49 }' | sed 's/<.*//')
-DF7=$(grep 'Downstream 7' $TEMP | awk -F'<td>' '{ print $58 }' | sed 's/<.*//')
-DF8=$(grep 'Downstream 8' $TEMP | awk -F'<td>' '{ print $67 }' | sed 's/<.*//')
+DF1=$(head -33 $TEMP | tail -1 | grep 'Hz' | awk -F'<TD>' '{ print $2 }' | sed 's/ Hz.*//')
+DF2=$(head -33 $TEMP | tail -1 | grep 'Hz' | awk -F'<TD>' '{ print $3 }' | sed 's/ Hz.*//')
+DF3=$(head -33 $TEMP | tail -1 | grep 'Hz' | awk -F'<TD>' '{ print $4 }' | sed 's/ Hz.*//')
+DF4=$(head -33 $TEMP | tail -1 | grep 'Hz' | awk -F'<TD>' '{ print $5 }' | sed 's/ Hz.*//')
+DF5=$(head -33 $TEMP | tail -1 | grep 'Hz' | awk -F'<TD>' '{ print $6 }' | sed 's/ Hz.*//')
+DF6=$(head -33 $TEMP | tail -1 | grep 'Hz' | awk -F'<TD>' '{ print $7 }' | sed 's/ Hz.*//')
+DF7=$(head -33 $TEMP | tail -1 | grep 'Hz' | awk -F'<TD>' '{ print $8 }' | sed 's/ Hz.*//')
+DF8=$(head -33 $TEMP | tail -1 | grep 'Hz' | awk -F'<TD>' '{ print $9 }' | sed 's/ Hz.*//')
 
 # downstream power
-DP1=$(grep "$DF1" $TEMP | awk -F'<td>' '{ print $5 }' | sed 's| dBmV.*||')
-DP2=$(grep "$DF2" $TEMP | awk -F'<td>' '{ print $14 }' | sed 's| dBmV.*||')
-DP3=$(grep "$DF3" $TEMP | awk -F'<td>' '{ print $23 }' | sed 's| dBmV.*||')
-DP4=$(grep "$DF4" $TEMP | awk -F'<td>' '{ print $32 }' | sed 's| dBmV.*||')
-DP5=$(grep "$DF5" $TEMP | awk -F'<td>' '{ print $41 }' | sed 's| dBmV.*||')
-DP6=$(grep "$DF6" $TEMP | awk -F'<td>' '{ print $50 }' | sed 's| dBmV.*||')
-DP7=$(grep "$DF7" $TEMP | awk -F'<td>' '{ print $59 }' | sed 's| dBmV.*||')
-DP8=$(grep "$DF8" $TEMP | awk -F'<td>' '{ print $68 }' | sed 's| dBmV.*||')
+DP1=$(head -36 $TEMP | tail -1 | grep 'dBmV' | awk -F'<TD>' '{ print $2 }' | sed 's| dBmV.*||')
+DP2=$(head -37 $TEMP | tail -1 | grep 'dBmV' | awk -F'<TD>' '{ print $2 }' | sed 's| dBmV.*||')
+DP3=$(head -38 $TEMP | tail -1 | grep 'dBmV' | awk -F'<TD>' '{ print $2 }' | sed 's| dBmV.*||')
+DP4=$(head -39 $TEMP | tail -1 | grep 'dBmV' | awk -F'<TD>' '{ print $2 }' | sed 's| dBmV.*||')
+DP5=$(head -40 $TEMP | tail -1 | grep 'dBmV' | awk -F'<TD>' '{ print $2 }' | sed 's| dBmV.*||')
+DP6=$(head -41 $TEMP | tail -1 | grep 'dBmV' | awk -F'<TD>' '{ print $2 }' | sed 's| dBmV.*||')
+DP7=$(head -42 $TEMP | tail -1 | grep 'dBmV' | awk -F'<TD>' '{ print $2 }' | sed 's| dBmV.*||')
+DP8=$(head -43 $TEMP | tail -1 | grep 'dBmV' | awk -F'<TD>' '{ print $2 }' | sed 's| dBmV.*||')
 
 # downstream snr
-DS1=$(grep "$DF1" $TEMP | awk -F'<td>' '{ print $6 }' | sed 's| dB.*||')
-DS2=$(grep "$DF2" $TEMP | awk -F'<td>' '{ print $15 }' | sed 's| dB.*||')
-DS3=$(grep "$DF3" $TEMP | awk -F'<td>' '{ print $24 }' | sed 's| dB.*||')
-DS4=$(grep "$DF4" $TEMP | awk -F'<td>' '{ print $33 }' | sed 's| dB.*||')
-DS5=$(grep "$DF5" $TEMP | awk -F'<td>' '{ print $42 }' | sed 's| dB.*||')
-DS6=$(grep "$DF6" $TEMP | awk -F'<td>' '{ print $51 }' | sed 's| dB.*||')
-DS7=$(grep "$DF7" $TEMP | awk -F'<td>' '{ print $60 }' | sed 's| dB.*||')
-DS8=$(grep "$DF8" $TEMP | awk -F'<td>' '{ print $69 }' | sed 's| dB.*||')
+DS1=$(head -34 $TEMP | tail -1 | grep 'dB' | awk -F'<TD>' '{ print $2 }' | sed 's| dB.*||')
+DS2=$(head -34 $TEMP | tail -1 | grep 'dB' | awk -F'<TD>' '{ print $3 }' | sed 's| dB.*||')
+DS3=$(head -34 $TEMP | tail -1 | grep 'dB' | awk -F'<TD>' '{ print $4 }' | sed 's| dB.*||')
+DS4=$(head -34 $TEMP | tail -1 | grep 'dB' | awk -F'<TD>' '{ print $5 }' | sed 's| dB.*||')
+DS5=$(head -34 $TEMP | tail -1 | grep 'dB' | awk -F'<TD>' '{ print $6 }' | sed 's| dB.*||')
+DS6=$(head -34 $TEMP | tail -1 | grep 'dB' | awk -F'<TD>' '{ print $7 }' | sed 's| dB.*||')
+DS7=$(head -34 $TEMP | tail -1 | grep 'dB' | awk -F'<TD>' '{ print $8 }' | sed 's| dB.*||')
+DS8=$(head -34 $TEMP | tail -1 | grep 'dB' | awk -F'<TD>' '{ print $9 }' | sed 's| dB.*||')
+
+# downstream unerrored codewords
+DUC1=$(head -83 $TEMP | tail -1 | awk -F'<TD>' '{ print $2 }' | sed 's|&nbsp.*||')
+DUC2=$(head -83 $TEMP | tail -1 | awk -F'<TD>' '{ print $3 }' | sed 's|&nbsp.*||')
+DUC3=$(head -83 $TEMP | tail -1 | awk -F'<TD>' '{ print $4 }' | sed 's|&nbsp.*||')
+DUC4=$(head -83 $TEMP | tail -1 | awk -F'<TD>' '{ print $5 }' | sed 's|&nbsp.*||')
+DUC5=$(head -83 $TEMP | tail -1 | awk -F'<TD>' '{ print $6 }' | sed 's|&nbsp.*||')
+DUC6=$(head -83 $TEMP | tail -1 | awk -F'<TD>' '{ print $7 }' | sed 's|&nbsp.*||')
+DUC7=$(head -83 $TEMP | tail -1 | awk -F'<TD>' '{ print $8 }' | sed 's|&nbsp.*||')
+DUC8=$(head -83 $TEMP | tail -1 | awk -F'<TD>' '{ print $9 }' | sed 's|&nbsp.*||')
+
+# downstream correctable codewords
+DCC1=$(head -84 $TEMP | tail -1 | awk -F'<TD>' '{ print $2 }' | sed 's|&nbsp.*||')
+DCC2=$(head -84 $TEMP | tail -1 | awk -F'<TD>' '{ print $3 }' | sed 's|&nbsp.*||')
+DCC3=$(head -84 $TEMP | tail -1 | awk -F'<TD>' '{ print $4 }' | sed 's|&nbsp.*||')
+DCC4=$(head -84 $TEMP | tail -1 | awk -F'<TD>' '{ print $5 }' | sed 's|&nbsp.*||')
+DCC5=$(head -84 $TEMP | tail -1 | awk -F'<TD>' '{ print $6 }' | sed 's|&nbsp.*||')
+DCC6=$(head -84 $TEMP | tail -1 | awk -F'<TD>' '{ print $7 }' | sed 's|&nbsp.*||')
+DCC7=$(head -84 $TEMP | tail -1 | awk -F'<TD>' '{ print $8 }' | sed 's|&nbsp.*||')
+DCC8=$(head -84 $TEMP | tail -1 | awk -F'<TD>' '{ print $9 }' | sed 's|&nbsp.*||')
+
+# downstream uncorrectable codewords
+DEC1=$(head -85 $TEMP | tail -1 | awk -F'<TD>' '{ print $2 }' | sed 's|&nbsp.*||')
+DEC2=$(head -85 $TEMP | tail -1 | awk -F'<TD>' '{ print $3 }' | sed 's|&nbsp.*||')
+DEC3=$(head -85 $TEMP | tail -1 | awk -F'<TD>' '{ print $4 }' | sed 's|&nbsp.*||')
+DEC4=$(head -85 $TEMP | tail -1 | awk -F'<TD>' '{ print $5 }' | sed 's|&nbsp.*||')
+DEC5=$(head -85 $TEMP | tail -1 | awk -F'<TD>' '{ print $6 }' | sed 's|&nbsp.*||')
+DEC6=$(head -85 $TEMP | tail -1 | awk -F'<TD>' '{ print $7 }' | sed 's|&nbsp.*||')
+DEC7=$(head -85 $TEMP | tail -1 | awk -F'<TD>' '{ print $8 }' | sed 's|&nbsp.*||')
+DEC8=$(head -85 $TEMP | tail -1 | awk -F'<TD>' '{ print $9 }' | sed 's|&nbsp.*||')
 
 # find upstream frequencies
-# note my modem does not show a value for Upstream 2
-# so use a different strategy
-
-UF1=$(grep '<td>Upstream' $TEMP | awk -F'<td>' '{ print $4 }' | sed 's/<.*//')
-UF2=$(grep '<td>Upstream' $TEMP | awk -F'<td>' '{ print $11 }' | sed 's/<.*//')
-UF3=$(grep '<td>Upstream' $TEMP | awk -F'<td>' '{ print $18 }' | sed 's/<.*//')
+UF1=$(head -57 $TEMP | tail -1 | grep 'Hz' | awk -F'<TD>' '{ print $2 }' | sed 's/ Hz.*//')
+UF2=$(head -57 $TEMP | tail -1 | grep 'Hz' | awk -F'<TD>' '{ print $3 }' | sed 's/ Hz.*//')
+UF3=$(head -57 $TEMP | tail -1 | grep 'Hz' | awk -F'<TD>' '{ print $4 }' | sed 's/ Hz.*//')
+UF4=$(head -57 $TEMP | tail -1 | grep 'Hz' | awk -F'<TD>' '{ print $5 }' | sed 's/ Hz.*//')
 
 # upstream power
-UP1=$(grep "$UF1" $TEMP | awk -F'<td>' '{ print $5 }' | sed 's| dBmV.*||')
-UP2=$(grep "$UF2" $TEMP | awk -F'<td>' '{ print $12 }' | sed 's| dBmV.*||')
-UP3=$(grep "$UF3" $TEMP | awk -F'<td>' '{ print $19 }' | sed 's| dBmV.*||')
+UP1=$(head -60 $TEMP | tail -1 | grep 'dBmV' | awk -F'<TD>' '{ print $2 }' | sed 's| dBmV.*||')
+UP2=$(head -60 $TEMP | tail -1 | grep 'dBmV' | awk -F'<TD>' '{ print $3 }' | sed 's| dBmV.*||')
+UP3=$(head -60 $TEMP | tail -1 | grep 'dBmV' | awk -F'<TD>' '{ print $4 }' | sed 's| dBmV.*||')
+UP4=$(head -60 $TEMP | tail -1 | grep 'dBmV' | awk -F'<TD>' '{ print $5 }' | sed 's| dBmV.*||')
+
 
 # force a 0 value when undefined due to poor connectivity
-[[ "$DF1" = "----</td>" ]] && DF1=0
-[[ "$DF2" = "----</td>" ]] && DF2=0
-[[ "$DF3" = "----</td>" ]] && DF3=0
-[[ "$DF4" = "----</td>" ]] && DF4=0
-[[ "$DF5" = "----</td>" ]] && DF5=0
-[[ "$DF6" = "----</td>" ]] && DF6=0
-[[ "$DF7" = "----</td>" ]] && DF7=0
-[[ "$DF8" = "----</td>" ]] && DF8=0
-[[ "$DP1" = "----</td>" ]] && DP1=0
-[[ "$DP2" = "----</td>" ]] && DP2=0
-[[ "$DP3" = "----</td>" ]] && DP3=0
-[[ "$DP4" = "----</td>" ]] && DP4=0
-[[ "$DP5" = "----</td>" ]] && DP5=0
-[[ "$DP6" = "----</td>" ]] && DP6=0
-[[ "$DP7" = "----</td>" ]] && DP7=0
-[[ "$DP8" = "----</td>" ]] && DP8=0
+[[ "$DF1" = "----</TD>" ]] && DF1=0
+[[ "$DF2" = "----</TD>" ]] && DF2=0
+[[ "$DF3" = "----</TD>" ]] && DF3=0
+[[ "$DF4" = "----</TD>" ]] && DF4=0
+[[ "$DF5" = "----</TD>" ]] && DF5=0
+[[ "$DF6" = "----</TD>" ]] && DF6=0
+[[ "$DF7" = "----</TD>" ]] && DF7=0
+[[ "$DF8" = "----</TD>" ]] && DF8=0
 
-[[ "$DS1" = "----</td>" ]] && DS1=0
-[[ "$DS2" = "----</td>" ]] && DS2=0
-[[ "$DS3" = "----</td>" ]] && DS3=0
-[[ "$DS4" = "----</td>" ]] && DS4=0
-[[ "$DS5" = "----</td>" ]] && DS5=0
-[[ "$DS6" = "----</td>" ]] && DS6=0
-[[ "$DS7" = "----</td>" ]] && DS7=0
-[[ "$DS8" = "----</td>" ]] && DS8=0
+[[ "$DP1" = "----</TD>" ]] && DP1=0
+[[ "$DP2" = "----</TD>" ]] && DP2=0
+[[ "$DP3" = "----</TD>" ]] && DP3=0
+[[ "$DP4" = "----</TD>" ]] && DP4=0
+[[ "$DP5" = "----</TD>" ]] && DP5=0
+[[ "$DP6" = "----</TD>" ]] && DP6=0
+[[ "$DP7" = "----</TD>" ]] && DP7=0
+[[ "$DP8" = "----</TD>" ]] && DP8=0
 
-[[ "$UF1" = "----</td>" ]] && UF1=0
-[[ "$UF2" = "----</td>" ]] && UF2=0
-[[ "$UF3" = "----</td>" ]] && UF3=0
+[[ "$DS1" = "----</TD>" ]] && DS1=0
+[[ "$DS2" = "----</TD>" ]] && DS2=0
+[[ "$DS3" = "----</TD>" ]] && DS3=0
+[[ "$DS4" = "----</TD>" ]] && DS4=0
+[[ "$DS5" = "----</TD>" ]] && DS5=0
+[[ "$DS6" = "----</TD>" ]] && DS6=0
+[[ "$DS7" = "----</TD>" ]] && DS7=0
+[[ "$DS8" = "----</TD>" ]] && DS8=0
+
+[[ "$UF1" = "----</TD>" ]] && UF1=0
+[[ "$UF2" = "----</TD>" ]] && UF2=0
+[[ "$UF3" = "----</TD>" ]] && UF3=0
+[[ "$UF4" = "----</TD>" ]] && UF4=0
 [[ -z "$UF1" ]] && UF1=0
 [[ -z "$UF2" ]] && UF2=0
 [[ -z "$UF3" ]] && UF3=0
+[[ -z "$UF4" ]] && UF4=0
 
-[[ "$UP1" = "----</td>" ]] && UP1=0
-[[ "$UP2" = "----</td>" ]] && UP2=0
-[[ "$UP3" = "----</td>" ]] && UP3=0
+[[ "$UP1" = "----</TD>" ]] && UP1=0
+[[ "$UP2" = "----</TD>" ]] && UP2=0
+[[ "$UP3" = "----</TD>" ]] && UP3=0
+[[ "$UP4" = "----</TD>" ]] && UP4=0
 [[ -z "$UP1" ]] && UP1=0
 [[ -z "$UP2" ]] && UP2=0
 [[ -z "$UP3" ]] && UP3=0
+[[ -z "$UP4" ]] && UP4=0
 
+
+# Test output
+echo "Downstream freq: $DF1, $DF2, $DF3, $DF4, $DF5, $DF6, $DF7, $DF8"
+echo "Downstream power: $DP1, $DP2, $DP3, $DP4, $DP5, $DP6, $DP7, $DP8"
+echo "Downstream SNR: $DS1, $DS2, $DS3, $DS4, $DS5, $DS6, $DS7, $DS8"
+echo ""
+echo "Upstream freq: $UF1, $UF2, $UF3, $UF4"
+echo "Upstream power: $UP1, $UP2, $UP3, $UP4"
+echo ""
+echo "Downstream Unerrored Codewords: $DUC1, $DUC2, $DUC3, $DUC4, $DUC5, $DUC6, $DUC7, $DUC8"
+echo "Downstream Correctable Codewords: $DCC1, $DCC2, $DCC3, $DCC4, $DCC5, $DCC6, $DCC7, $DCC8"
+echo "Downstream Uncorrectable Codewords: $DEC1, $DEC2, $DEC3, $DEC4, $DEC5, $DEC6, $DEC7, $DEC8"
+
+
+# Write to InfluxDB TSDB
+curl -s -i -XPOST 'http://localhost:8086/write?db=arris_stats' --data-binary "downstream_freq,channel=1 value=$DF1
+downstream_freq,channel=2 value=$DF2
+downstream_freq,channel=3 value=$DF3
+downstream_freq,channel=4 value=$DF4
+downstream_freq,channel=5 value=$DF5
+downstream_freq,channel=6 value=$DF6
+downstream_freq,channel=7 value=$DF7
+downstream_freq,channel=8 value=$DF8
+downstream_power,channel=1 value=$DP1
+downstream_power,channel=2 value=$DP2
+downstream_power,channel=3 value=$DP3
+downstream_power,channel=4 value=$DP4
+downstream_power,channel=5 value=$DP5
+downstream_power,channel=6 value=$DP6
+downstream_power,channel=7 value=$DP7
+downstream_power,channel=8 value=$DP8
+downstream_snr,channel=1 value=$DS1
+downstream_snr,channel=2 value=$DS2
+downstream_snr,channel=3 value=$DS3
+downstream_snr,channel=4 value=$DS4
+downstream_snr,channel=5 value=$DS5
+downstream_snr,channel=6 value=$DS6
+downstream_snr,channel=7 value=$DS7
+downstream_snr,channel=8 value=$DS8
+downstream_unerrored_codewords,channel=1 value=$DUC1
+downstream_unerrored_codewords,channel=2 value=$DUC2
+downstream_unerrored_codewords,channel=3 value=$DUC3
+downstream_unerrored_codewords,channel=4 value=$DUC4
+downstream_unerrored_codewords,channel=5 value=$DUC5
+downstream_unerrored_codewords,channel=6 value=$DUC6
+downstream_unerrored_codewords,channel=7 value=$DUC7
+downstream_unerrored_codewords,channel=8 value=$DUC8
+downstream_correctable_codewords,channel=1 value=$DCC1
+downstream_correctable_codewords,channel=2 value=$DCC2
+downstream_correctable_codewords,channel=3 value=$DCC3
+downstream_correctable_codewords,channel=4 value=$DCC4
+downstream_correctable_codewords,channel=5 value=$DCC5
+downstream_correctable_codewords,channel=6 value=$DCC6
+downstream_correctable_codewords,channel=7 value=$DCC7
+downstream_correctable_codewords,channel=8 value=$DCC8
+downstream_uncorrectable_codewords,channel=1 value=$DEC1
+downstream_uncorrectable_codewords,channel=2 value=$DEC2
+downstream_uncorrectable_codewords,channel=3 value=$DEC3
+downstream_uncorrectable_codewords,channel=4 value=$DEC4
+downstream_uncorrectable_codewords,channel=5 value=$DEC5
+downstream_uncorrectable_codewords,channel=6 value=$DEC6
+downstream_uncorrectable_codewords,channel=7 value=$DEC7
+downstream_uncorrectable_codewords,channel=8 value=$DEC8
+upstream_freq,channel=1 value=$UF1
+upstream_freq,channel=2 value=$UF2
+upstream_freq,channel=3 value=$UF3
+upstream_freq,channel=4 value=$UF4
+upstream_power,channel=1 value=$UP1
+upstream_power,channel=2 value=$UP2
+upstream_power,channel=3 value=$UP3
+upstream_power,channel=4 value=$UP4" >/dev/null
+
+
+# The old CSV format logs from the original script
+#
 # The individual log files
-DLOGFREQ="$LOGPATH/downstream-freq.csv"
-DLOGPOWER="$LOGPATH/downstream-power.csv"
-DLOGSNR="$LOGPATH/downstream-SNR.csv"
-ULOGFREQ="$LOGPATH/upstream-freq.csv"
-ULOGPOWER="$LOGPATH/upstream-power.csv"
+# DLOGFREQ="$LOGPATH/downstream-freq.csv"
+# DLOGPOWER="$LOGPATH/downstream-power.csv"
+# DLOGSNR="$LOGPATH/downstream-SNR.csv"
+# ULOGFREQ="$LOGPATH/upstream-freq.csv"
+# ULOGPOWER="$LOGPATH/upstream-power.csv"
 
-# downstream frequency log
-[[ ! -f $DLOGFREQ ]] && echo "DTS,Downstream 1,Downstream 2,Downstream 3,Downstream 4,Downstream 5,Downstream 6,Downstream 7,Downstream 8" > $DLOGFREQ
-echo "$RUNDATE,${DF1/ *},${DF2/ *},${DF3/ *},${DF4/ *},${DF5/ *},${DF6/ *},${DF7/ *},${DF8/ *}" >> $DLOGFREQ
+# # # downstream frequency log
+# [[ ! -f $DLOGFREQ ]] && echo "DTS,Downstream 1,Downstream 2,Downstream 3,Downstream 4,Downstream 5,Downstream 6,Downstream 7,Downstream 8" > $DLOGFREQ
+# echo "$RUNDATE,${DF1/ *},${DF2/ *},${DF3/ *},${DF4/ *},${DF5/ *},${DF6/ *},${DF7/ *},${DF8/ *}" >> $DLOGFREQ
 
-# downstream power log
-[[ ! -f $DLOGPOWER ]] && echo "DTS,Downstream 1,Downstream 2,Downstream 3,Downstream 4,Downstream 5,Downstream 6,Downstream 7,Downstream 8" > $DLOGPOWER
-echo "$RUNDATE,$DP1,$DP2,$DP3,$DP4,$DP5,$DP6,$DP7,$DP8" >> $DLOGPOWER
+# # # downstream power log
+# [[ ! -f $DLOGPOWER ]] && echo "DTS,Downstream 1,Downstream 2,Downstream 3,Downstream 4,Downstream 5,Downstream 6,Downstream 7,Downstream 8" > $DLOGPOWER
+# echo "$RUNDATE,$DP1,$DP2,$DP3,$DP4,$DP5,$DP6,$DP7,$DP8" >> $DLOGPOWER
 
-# downstream SNR log
-[[ ! -f $DLOGSNR ]] && echo "DTS,Downstream 1,Downstream 2,Downstream 3,Downstream 4,Downstream 5,Downstream 6,Downstream 7,Downstream 8" > $DLOGSNR
-echo "$RUNDATE,$DS1,$DS2,$DS3,$DS4,$DS5,$DS6,$DS7,$DS8" >> $DLOGSNR
+# # # downstream SNR log
+# [[ ! -f $DLOGSNR ]] && echo "DTS,Downstream 1,Downstream 2,Downstream 3,Downstream 4,Downstream 5,Downstream 6,Downstream 7,Downstream 8" > $DLOGSNR
+# echo "$RUNDATE,$DS1,$DS2,$DS3,$DS4,$DS5,$DS6,$DS7,$DS8" >> $DLOGSNR
 
-# upstream freq log
-[[ ! -f $ULOGFREQ ]] && echo "DTS,Upstream 1,Upstream 2,Upstream 3" > $ULOGFREQ
-echo "$RUNDATE,${UF1/ *},${UF2/ *},${UF3/ *}" >> $ULOGFREQ
+# # # upstream freq log
+# [[ ! -f $ULOGFREQ ]] && echo "DTS,Upstream 1,Upstream 2,Upstream 3,Upstream 4" > $ULOGFREQ
+# echo "$RUNDATE,${UF1/ *},${UF2/ *},${UF3/ *},${UF4/ *}" >> $ULOGFREQ
 
-# upstream power log
-[[ ! -f $ULOGPOWER ]] && echo "DTS,Upstream 1,Upstream 2,Upstream 3" > $ULOGPOWER
-echo "$RUNDATE,$UP1,$UP2,$UP3" >> $ULOGPOWER
+# # # upstream power log
+# [[ ! -f $ULOGPOWER ]] && echo "DTS,Upstream 1,Upstream 2,Upstream 3,Upstream 4" > $ULOGPOWER
+# echo "$RUNDATE,$UP1,$UP2,$UP3,$UP4" >> $ULOGPOWER
